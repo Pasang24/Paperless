da4a8bd15e65b30598ee434487d645bcfe8406c0
Pasang24 <pasangtheeng14@gmail.com>
2025-05-16 19:05:20 +0545

    feat: add functionality to add, remove and change the option in checkbox field

--- src/components/FormCheckboxField.tsx
+++ src/components/FormCheckboxField.tsx
@@ -3,6 +3,7 @@ import { FormCheckBox, FormInputSchema } from "@/types/form";
 import { Input } from "./ui/input";
 import { Square, X } from "lucide-react";
 import { Button } from "./ui/button";
+import { nanoid } from "nanoid";
 
 interface FormCheckboxFieldProps {
   formInput: FormCheckBox;
@@ -13,18 +14,96 @@ function FormCheckboxField({
   formInput,
   changeFormInput,
 }: FormCheckboxFieldProps) {
+  const isFormCheckbox = (
+    schema: FormInputSchema | FormCheckBox
+  ): schema is FormCheckBox => {
+    return schema.id === formInput.id;
+  };
+
+  const handleAddOption = () => {
+    changeFormInput((prevFormSchema) => {
+      return prevFormSchema.map((schema) => {
+        return isFormCheckbox(schema)
+          ? {
+              ...schema,
+              options: [
+                ...schema.options,
+                { id: nanoid(3), option: "Add Option" },
+              ],
+            }
+          : schema;
+      });
+    });
+  };
+
+  const handleRemoveOption = (optionId: string) => {
+    changeFormInput((prevFormSchema) => {
+      return prevFormSchema.map((schema) => {
+        if (isFormCheckbox(schema)) {
+          const optionIndex = schema.options.findIndex(
+            (option) => option.id === optionId
+          );
+
+          if (schema.options.length === 1) return schema;
+
+          const leftHalf = schema.options.slice(0, optionIndex);
+          const rightHalf = schema.options.slice(optionIndex + 1);
+
+          return { ...schema, options: [...leftHalf, ...rightHalf] };
+        } else {
+          return schema;
+        }
+      });
+    });
+  };
+
+  const handleChangeOption = (optionId: string, newOptionValue: string) => {
+    changeFormInput((prevFormSchema) => {
+      return prevFormSchema.map((schema) => {
+        if (isFormCheckbox(schema)) {
+          const optionIndex = schema.options.findIndex(
+            (option) => option.id === optionId
+          );
+
+          const leftHalf = schema.options.slice(0, optionIndex);
+          const rightHalf = schema.options.slice(optionIndex + 1);
+
+          return {
+            ...schema,
+            options: [
+              ...leftHalf,
+              { ...schema.options[optionIndex], option: newOptionValue },
+              ...rightHalf,
+            ],
+          };
+        } else {
+          return schema;
+        }
+      });
+    });
+  };
   return (
     <div className="flex flex-col gap-2">
       {formInput.options.map((option) => (
         <div key={option.id} className="flex items-center gap-2">
           <Square stroke="gray" />
-          <Input value={option.option} />
-          <Button variant={"ghost"} size={"icon"} className="rounded-full">
+          <Input
+            value={option.option}
+            onChange={(event) =>
+              handleChangeOption(option.id, event.target.value)
+            }
+          />
+          <Button
+            onClick={() => handleRemoveOption(option.id)}
+            variant={"ghost"}
+            size={"icon"}
+            className="rounded-full"
+          >
             <X />
           </Button>
         </div>
       ))}
-      <Button variant={"ghost"} className="self-end">
+      <Button onClick={handleAddOption} variant={"ghost"} className="self-end">
         + Add Option
       </Button>
     </div>
