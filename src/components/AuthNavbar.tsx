b75c609b88d4cb5d2c4ac02585f4b3c6ac7fcf27
Pasang24 <pasangtheeng14@gmail.com>
2025-05-23 15:03:35 +0545

    style: change the padding in navbar and button text weight

--- src/components/AuthNavbar.tsx
+++ src/components/AuthNavbar.tsx
@@ -3,16 +3,24 @@ import { buttonVariants } from "./ui/button";
 
 function AuthNavbar() {
   return (
-    <nav className="flex justify-between items-center p-2 sm:px-4 border-b">
+    <nav className="flex justify-between items-center px-2 py-4 sm:px-4 border-b">
       <Link href="/" className="text-2xl font-semibold">
         Paperless
       </Link>
       <div className="flex items-center gap-2">
-        <Link href="/login" className={buttonVariants({ variant: "ghost" })}>
-          Login
+        <Link
+          href="/login"
+          className={`${buttonVariants({ variant: "ghost" })} font-semibold`}
+        >
+          Log in
         </Link>
-        <Link href="/signup" className={buttonVariants({ variant: "default" })}>
-          Signup
+        <Link
+          href="/signup"
+          className={`${buttonVariants({
+            variant: "default",
+          })} font-semibold`}
+        >
+          Sign up
         </Link>
       </div>
     </nav>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import NavProfile from "./NavProfile";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-2 sm:px-4 border-b">
+    <nav className="flex justify-between items-center px-2 py-4 sm:px-4 border-b">
       <Link href="/forms" className="text-2xl font-semibold">
         Paperless
       </Link>
